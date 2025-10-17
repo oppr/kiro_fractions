33fbae29010dbe8b72ebf9b51a001651ec088cf7
Roger Opperman <12088+oppr@users.noreply.github.com>
2025-10-17 18:20:01 -0500

    Trying to deploy to GH pages with a GH action

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kiro_fractions/',
   test: {
     globals: true,
     environment: 'jsdom',
